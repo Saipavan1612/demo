c275ab509b95c955d4c00ffc28e0d0bf37559bb0
Gurram Saipavan <gurramsaipavan@Innbuilts-MacBook-Air.local>
2018-03-14 12:18:20 +0530

    Modified

--- update.php
+++ update.php
@@ -1,4 +1,35 @@
 <?php
 require('connect_mysql.php');
-?>
+$id = $_GET['id'];
+echo $id;
+$sql = "SELECT * FROM students where id='$id'";
+$res = $conn->query($sql);
+if($res->num_rows > 0) {
+    $row = $res->fetch_assoc();
 
+?>
+<center>
+    <form>
+        <table>
+            <tr>
+                <td><b>ID:</b></td>
+                <td><input type="text" value="<?php echo $row['id']?>"></td>
+            </tr>
+            <tr>
+                <td><b>Name:</b></td>
+                <td><input type="text" value="<?php echo $row['name']?>"></td>
+            </tr>
+            <tr>
+                <td><b>Email:</b></td>
+                <td><input type="email" value="<?php echo $row['email'] ?>"</td>
+            </tr>
+            <tr>
+                <td><input type="submit" value="Update"></td>
+                <td><inpu type="reset" value="Cancel"></td>
+            </tr>
+        </table>
+    </form>
+</center>
+<?php
+}
+?>
---
+++ update_data.php
@@ -0,0 +1,3 @@
+<?php
+require('connect_mysql.php');
+
